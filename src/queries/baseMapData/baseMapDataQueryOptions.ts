a61ab5766adb1999bd121eeb9faaffce9bce6f13
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-05 16:44:09 -0400

    Add consistent query key

--- src/queries/baseMapData/baseMapDataQueryOptions.ts
+++ src/queries/baseMapData/baseMapDataQueryOptions.ts
@@ -1,9 +1,11 @@
 import { queryOptions } from '@tanstack/react-query'
 import { fetchBaseMapData } from './baseMapData'
 
+export const baseMapDataKey = 'base-map-data'
+
 export default function baseMapDataQueryOptions() {
   return queryOptions({
-    queryKey: ['base-map-data'],
+    queryKey: [baseMapDataKey],
     queryFn: () => fetchBaseMapData(),
   })
 }
