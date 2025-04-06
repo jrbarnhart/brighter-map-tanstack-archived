139ba364a20334195599f633ce758a3bc7bcca0b
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-05 20:34:35 -0400

    Pass enabled value

--- src/queries/searchData/searchDataQueryOptions.ts
+++ src/queries/searchData/searchDataQueryOptions.ts
@@ -3,8 +3,10 @@ import { fetchSearchData } from './searchData'
 
 export const searchDataKey = 'search-data'
 
-export const searchDataQueryOptions = queryOptions({
-  queryKey: [searchDataKey],
-  staleTime: 1800000, // 30 min since data set is large and relatively unchanging
-  queryFn: () => fetchSearchData(),
-})
+export const searchDataQueryOptions = (enabled: boolean) =>
+  queryOptions({
+    queryKey: [searchDataKey],
+    staleTime: 1800000, // 30 min since data set is large and relatively unchanging
+    queryFn: () => fetchSearchData(),
+    enabled,
+  })
