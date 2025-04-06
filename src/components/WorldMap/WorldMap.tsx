e8c86ba636a758506ee749ad95d54722f4612915
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-06 16:19:18 -0400

    Generate world shapes and add to canvas

--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -1,14 +1,23 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import MapRoom from './MapRoom'
+import roomRenderData from '@/lib/mapData/roomRenderData'
+import { useMemo } from 'react'
+import generateRoomShapes from './generateRoomShapes'
 
 export default function WorldMap() {
+  const shapes = useMemo(() => {
+    return generateRoomShapes(roomRenderData)
+  }, [])
+
   return (
     <div id="canvas-container" className="h-full w-full">
-      <Canvas camera={{ position: [0, 0, 20], fov: 50 }}>
+      <Canvas
+        camera={{ position: [0, 0, 20], fov: 50 }}
+        className="bg-stone-900"
+      >
         <ambientLight intensity={0.5} />
-        <OrbitControls />
-        <MapRoom />
+        <OrbitControls enableRotate={false} />
+        {shapes}
       </Canvas>
     </div>
   )
