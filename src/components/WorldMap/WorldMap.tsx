0731b6a5f46372d805be87b3aab30a9fb5f72c6d
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-05 22:55:49 -0400

    Add basic canvas to draw map on

---
+++ src/components/WorldMap/WorldMap.tsx
@@ -0,0 +1,9 @@
+import { Canvas } from '@react-three/fiber'
+
+export default function WorldMap() {
+  return (
+    <div id="canvas-container" className="h-full w-full">
+      <Canvas className="bg-amber-400" />
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { baseMapDataQueryOptions } from '@/queries/baseMapData/baseMapDataQueryO
 import { useSuspenseQuery } from '@tanstack/react-query'
 
 import useLazyQueries from '@/lib/hooks/useLazyQueries'
+import WorldMap from '@/components/WorldMap/WorldMap'
 
 export const Route = createFileRoute('/')({
   loader: ({ context: { queryClient } }) =>
@@ -19,75 +20,8 @@ function App() {
   const { data: lazyData, handlers: lazyHandlers } = useLazyQueries()
 
   return (
-    <div className="flex flex-col gap-8 p-4">
-      <div>
-        <h1>Base Data:</h1>
-        <p>Total rooms: {baseMapData.rooms.length}</p>
-        <p>Total vendors: {baseMapData.vendors.length}</p>
-      </div>
-      <div className="flex flex-col gap-2">
-        <h1>Mock interactive elements:</h1>
-        <label htmlFor="search">
-          Search:
-          <input
-            type="search"
-            placeholder="Search..."
-            className="border border-gray-400"
-            onFocus={() => {
-              lazyHandlers.enableSearchData()
-            }}
-          />
-        </label>
-        <a
-          href="#"
-          onMouseOver={() => lazyHandlers.enableDropTablesData()}
-          className="bg-blue-500 w-min whitespace-nowrap p-2 font-bold rounded-xl"
-        >
-          Monster Variant Link
-        </a>
-      </div>
-      <div>
-        <h1>Lazy Data:</h1>
-        <p>
-          Armor Variants:{' '}
-          {lazyData.armorVariants
-            ? lazyData.armorVariants.length
-            : 'Not loaded'}
-        </p>
-        <p>
-          Weapon Variants:{' '}
-          {lazyData.weaponVariants
-            ? lazyData.weaponVariants.length
-            : 'Not loaded'}
-        </p>
-        <p>
-          Consumable Variants:{' '}
-          {lazyData.consumableVariants
-            ? lazyData.consumableVariants.length
-            : 'Not loaded'}
-        </p>
-        <p>
-          Misc Items:{' '}
-          {lazyData.miscItems ? lazyData.miscItems.length : 'Not loaded'}
-        </p>
-        <p>
-          Monster Variants:{' '}
-          {lazyData.monsterVariants
-            ? lazyData.monsterVariants.length
-            : 'Not loaded'}
-        </p>
-        <p>
-          Resource Variants:{' '}
-          {lazyData.resourceVariants
-            ? lazyData.resourceVariants.length
-            : 'Not loaded'}
-        </p>
-        <p>Quests: {lazyData.quests ? lazyData.quests.length : 'Not loaded'}</p>
-        <p>
-          Drop Tables:{' '}
-          {lazyData.dropTables ? lazyData.dropTables.length : 'Not loaded'}
-        </p>
-      </div>
+    <div id="app-container" className="h-svh w-svw">
+      <WorldMap />
     </div>
   )
 }
