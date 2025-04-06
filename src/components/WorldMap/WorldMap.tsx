9695e3246e1b2b9fe7779a0ff2ea194c998de3ca
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-06 16:34:22 -0400

    Change rendering mode for better performance

--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -1,8 +1,14 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import roomRenderData from '@/lib/mapData/roomRenderData'
 import { useMemo } from 'react'
 import generateRoomShapes from './generateRoomShapes'
+
+function Controls() {
+  const { invalidate } = useThree()
+
+  return <OrbitControls enableRotate={false} onChange={() => invalidate()} />
+}
 
 export default function WorldMap() {
   const shapes = useMemo(() => {
@@ -14,9 +20,11 @@ export default function WorldMap() {
       <Canvas
         camera={{ position: [0, 0, 20], fov: 50 }}
         className="bg-stone-900"
+        gl={{ alpha: false }}
+        frameloop="demand"
       >
         <ambientLight intensity={0.5} />
-        <OrbitControls enableRotate={false} />
+        <Controls />
         {shapes}
       </Canvas>
     </div>
