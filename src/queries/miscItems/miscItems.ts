8ae7aa35a751e66a1bf6c44ea8b566df0b948be3
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-05 20:55:50 -0400

    Add query and options for misc items

---
+++ src/queries/miscItems/miscItems.ts
@@ -0,0 +1,22 @@
+import type { paths } from '@/lib/types/apiTypes'
+import { axiosClient } from '../axiosClient'
+import axios from 'axios'
+
+export async function fetchMiscItems() {
+  // console.info('Fetching misc items...');
+  try {
+    const miscItemsQuery =
+      await axiosClient.get<
+        paths['/items/misc']['get']['responses']['200']['content']['application/json']
+      >('/items/misc')
+
+    return { miscItems: miscItemsQuery.data }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message)
+    } else {
+      console.error(error)
+      throw new Error('An unexpected error occurred while fetching misc items.')
+    }
+  }
+}
---
+++ src/queries/miscItems/miscItemsQueryOptions.ts
@@ -0,0 +1,12 @@
+import { queryOptions } from '@tanstack/react-query'
+import { fetchMiscItems } from './miscItems'
+
+export const miscItemsKey = 'misc-items'
+
+export const dropTableQueryOptions = (enabled: boolean) =>
+  queryOptions({
+    queryKey: [miscItemsKey],
+    staleTime: 1800000, // 30 min since data set is large and relatively unchanging
+    queryFn: () => fetchMiscItems(),
+    enabled,
+  })
