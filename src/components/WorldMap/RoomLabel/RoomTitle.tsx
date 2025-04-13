ee332fa7d584bc0afd44182b6dac5a0d8c129327
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-13 05:34:31 -0400

    Adjust font size

--- src/components/WorldMap/RoomLabel/RoomTitle.tsx
+++ src/components/WorldMap/RoomLabel/RoomTitle.tsx
@@ -10,7 +10,7 @@ export default function RoomTitle({ name }: RoomTitleProps) {
   const OFFSET_Y = 0.2
   const BG_Z = 0
   const TEXT_Z = 0.01
-  const FONT_HEIGHT = 0.6
+  const FONT_HEIGHT = 0.5
   const AVERAGE_FONT_WIDTH = 0.6
   const PADDING_Y = 0.2
   const PADDING_X = 0.2
@@ -21,9 +21,8 @@ export default function RoomTitle({ name }: RoomTitleProps) {
 
     const lineLength = name.length
 
-    const textHeight = 0.5
     const paddingVertical = PADDING_Y * 2 // Top and bottom padding
-    const height = textHeight + paddingVertical
+    const height = FONT_HEIGHT + paddingVertical
 
     const paddingHorizontal = PADDING_X * 2 // Left and right padding (adjust as needed)
     const width =
