f23fcaccc216806a481ebde66c5eb0cdb8408b0e
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-13 03:47:23 -0400

    Install router lint plugin and config it

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 //  @ts-check
 import { tanstackConfig } from '@tanstack/eslint-config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import pluginRouter from '@tanstack/eslint-plugin-router'
 
 export default [
   // Add an ignores config at the beginning to exclude these files entirely
@@ -7,4 +9,6 @@ export default [
     ignores: ['vite.config.js', 'prettier.config.js', 'eslint.config.js'],
   },
   ...tanstackConfig,
+  ...pluginQuery.configs['flat/recommended'],
+  ...pluginRouter.configs['flat/recommended'],
 ]
