e3bf73b9e17b01b840607644af38932d7be647e8
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-13 03:43:47 -0400

    Dont lint config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
 //  @ts-check
-
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  // Add an ignores config at the beginning to exclude these files entirely
+  {
+    ignores: ['vite.config.js', 'prettier.config.js', 'eslint.config.js'],
+  },
+  ...tanstackConfig,
+]
