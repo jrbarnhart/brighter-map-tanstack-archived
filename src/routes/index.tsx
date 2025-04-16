a5a8ca39e76b1fc451b78c93adfd5f7276494f02
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 00:20:16 -0400

    Add info panel component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import WorldMap from '@/components/WorldMap/WorldMap'
 import FiltersPanel from '@/components/FiltersPanel/FiltersPanel'
 import { useState } from 'react'
 import MapSearchBar from '@/components/MapControls/MapControls'
+import InfoPanel from '@/components/InfoPanel/InfoPanel'
 
 export const Route = createFileRoute('/')({
   loader: ({ context: { queryClient } }) =>
@@ -29,13 +30,14 @@ function App() {
   return (
     <div id="app-container" className="relative h-svh w-svw flex">
       <FiltersPanel open={filtersOpen} setOpen={setFiltersOpen} />
-      <div className="w-full absolute top-0 left-0 z-10 p-8">
+      <div className="w-full absolute top-0 right-0 z-10 p-8">
         <MapSearchBar
           setFiltersOpen={setFiltersOpen}
           setInfoOpen={setInfoOpen}
         />
       </div>
       <WorldMap baseMapData={baseMapData} />
+      <InfoPanel open={infoOpen} setOpen={setInfoOpen} />
     </div>
   )
 }
