718ce5e80011d107c14adde598fcdc597780a71c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 00:36:31 -0400

    Fix panel slide in from right causing overflow

--- src/components/InfoPanel/InfoPanel.tsx
+++ src/components/InfoPanel/InfoPanel.tsx
@@ -12,17 +12,14 @@ export default function InfoPanel({ open, setOpen }: InfoPanelProps) {
   return (
     <aside
       className={cn(
-        'flex flex-col gap-3 p-6 w-full shrink-0 transition-transform duration-300 ease-in-out z-20',
-        'absolute top-0 right-0 bottom-0 bg-background border-r', // Mobile
-        open ? 'translate-x-0' : 'translate-x-full', // Translate if open
-        'md:w-96', // Desktop
+        'flex flex-col gap-3 p-6 shrink-0 transition-transform duration-300 ease-in-out z-20',
+        'absolute top-0 bottom-0 right-0 bg-background border-l md:w-96 w-full',
+        open ? 'translate-x-0' : 'translate-x-full',
       )}
     >
       <Button
         className="self-end w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-gray-200 hover:bg-gray-300"
-        onClick={() => {
-          setOpen(false)
-        }}
+        onClick={() => setOpen(false)}
         inert={!open ? true : undefined}
       >
         <ArrowRightToLine />
