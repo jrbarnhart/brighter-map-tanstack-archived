636d9d2f9f71b1e97302158f7fa747726b22d734
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-15 23:29:05 -0400

    Add map controls and state for info panel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import useLazyQueries from '@/lib/hooks/useLazyQueries'
 import WorldMap from '@/components/WorldMap/WorldMap'
 import FiltersPanel from '@/components/FiltersPanel/FiltersPanel'
 import { useState } from 'react'
+import MapSearchBar from '@/components/MapControls/MapControls'
 
 export const Route = createFileRoute('/')({
   loader: ({ context: { queryClient } }) =>
@@ -19,7 +20,8 @@ function App() {
   const baseMapData = baseMapQuery.data
 
   // Side panel state
-  const [filtersOpen, setFiltersOpen] = useState(true)
+  const [filtersOpen, setFiltersOpen] = useState(false)
+  const [infoOpen, setInfoOpen] = useState(false)
 
   // Lazy data loading
   const { data: lazyData, handlers: lazyHandlers } = useLazyQueries()
@@ -27,6 +29,12 @@ function App() {
   return (
     <div id="app-container" className="relative h-svh w-svw flex">
       <FiltersPanel open={filtersOpen} setOpen={setFiltersOpen} />
+      <div className="w-full absolute top-0 left-0 z-10 p-8">
+        <MapSearchBar
+          setFiltersOpen={setFiltersOpen}
+          setInfoOpen={setInfoOpen}
+        />
+      </div>
       <WorldMap baseMapData={baseMapData} />
     </div>
   )
